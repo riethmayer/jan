462756f77d695b8b3e3825aae134e147e568390a
Jan Riethmayer <jan@riethmayer.de>
2014-02-15 17:32:01 +0100

    initial commit of my blank sub.

--- completions/jan.bash
+++ completions/jan.bash
@@ -1,14 +1,14 @@
-_sub() {
+_jan() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
 
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(sub commands)" -- "$word") )
+    COMPREPLY=( $(compgen -W "$(jan commands)" -- "$word") )
   else
     local command="${COMP_WORDS[1]}"
-    local completions="$(sub completions "$command")"
+    local completions="$(jan completions "$command")"
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
 }
 
-complete -F _sub sub
+complete -F _jan jan
--- completions/jan.zsh
+++ completions/jan.zsh
@@ -2,17 +2,17 @@ if [[ ! -o interactive ]]; then
     return
 fi
 
-compctl -K _sub sub
+compctl -K _jan jan
 
-_sub() {
+_jan() {
   local word words completions
   read -cA words
   word="${words[2]}"
 
   if [ "${#words}" -eq 2 ]; then
-    completions="$(sub commands)"
+    completions="$(jan commands)"
   else
-    completions="$(sub completions "${word}")"
+    completions="$(jan completions "${word}")"
   fi
 
   reply=("${(ps:\n:)completions}")
